b519f7f7387345e10d85de85da4d02beab2fc1ee
Graham Blevins <hgblevins@gmail.com>
2016-10-29 14:29:43 -0400

    use bindShare instead of local cache

--- DatabaseServiceProvider.php
+++ DatabaseServiceProvider.php
@@ -4,12 +4,10 @@ namespace Expresser\Database;
 
 class DatabaseServiceProvider extends \Themosis\Core\IgniterService
 {
-    private static $connection;
-
     public function ignite()
     {
-        $this->app->bind('db', function ($app) {
-            return static::$connection ?: static::$connection = new WpdbConnection();
+        $this->app->bindShared('db', function ($app) {
+            return new WpdbConnection;
         });
     }
 }
